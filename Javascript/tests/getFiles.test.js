c2dbb65126ba69d3aff2cd85103c640781dc6323
Juanca <juancarabellino@gmail.com>
2021-12-07 13:56:41 -0300

    Agrego ejemplos de codigo javascript para dracul media y tests

--- Javascript/tests/getFiles.test.js
+++ Javascript/tests/getFiles.test.js
@@ -1,21 +1,35 @@
-const getFiles = require('../Javascript code examples/getFiles')
-const getFileById = require('../Javascript code examples/getFileById')
-const postFile = require('../Javascript code examples/postFile')
+//const callAPI = require('../HTTP-example')
+let callAPI = require('../HTTP-example')
 
 describe("Tests API dracul media code example ", () => {
-  test('getFileById', async () => {
-    let file = await getFileById
-      expect (file).toEqual({})
+
+  test('getFileWithoutHost', async () => {
+    let file = await callAPI('GET', null, 7070, '/api/file')
+      expect (file).toEqual({errorMessage: 'undefined host, port or path.'})
   }, 2000)
 
+  test('getFileWithoutPort', async () => {
+    let file = await callAPI('GET', '192.168.10.33', null, '/api/file')
+      expect (file).toEqual({errorMessage: 'undefined host, port or path.'})
+  }, 2000) 
+
+  test('getFileWithoutPath', async () => {
+    let file = await callAPI('GET', '192.168.10.33', 7070, null)
+      expect (file).toEqual({errorMessage: 'undefined host, port or path.'})
+  }, 2000) 
+
+  /* test('getFileById', async () => {
+    let file = await callAPI('GET', '192.168.10.33', 7070, '/api/file', '618aabf8fcce23001007d843')
+    expect (file).toEqual({})
+  }, 2000)
+  
   test('getFiles', async () => {
-    let files = await getFiles
+    let files = await callAPI('GET', '192.168.10.33', 7070, '/api/file')
       expect (files).toEqual({})
   }, 2000)
 
   test('postFile', async () => {
-    let file = await postFile
+    let file = callAPI('POST', '192.168.10.33', 7070, '/api/file', null, null, './exampleFiles/prueba.txt')
       expect (file).toEqual({})
-  }, 2000)
-  
+  }, 2000) */
 })
