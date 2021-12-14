ae2f252bca72024ae96a7bb7fa4338a09be62a6f
Juanca <juancarabellino@gmail.com>
2021-12-14 17:54:24 -0300

    Modifico pegadas a API en javascript y agrego tests

--- Javascript/HTTP-example.js
+++ Javascript/HTTP-example.js
@@ -1,7 +1,26 @@
-const callAPI = async (method, host, port, path, id = null, params = [], filePath = null, result) => {
-  if (!host || !port || !path) return { errorMessage: 'undefined host, port or path.' }
+const { get, request } = require('http')
+const FormData = require('form-data')
+const { createReadStream } = require('fs')
 
-  let tokenStr = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMjNjZjBhZDExM2E3MDAxMWQxNzI1OSIsInVzZXJuYW1lIjoicm9vdCIsInJvbGUiOnsiaWQiOiI2MTIzY2YwYWQxMTNhNzAwMTFkMTcyNDUiLCJuYW1lIjoiYWRtaW4iLCJjaGlsZFJvbGVzIjpudWxsfSwiZ3JvdXBzIjpbXSwiaWRTZXNzaW9uIjoiNjFhZWU0YzcxNTdjMTcwMDEwZGIyNDZkIiwiaWF0IjoxNjM4ODUxNzgzLCJleHAiOjE2Mzg5MzgxODMsImp0aSI6IjYxMjNjZjBhZDExM2E3MDAxMWQxNzI1OSJ9.lx_Gp41tu_yY_smcA78lunCo9fYneKRYAFqRzOmE4bE'
+let tokenStr = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMjNjZjBhZDExM2E3MDAxMWQxNzI1OSIsInVzZXJuYW1lIjoicm9vdCIsInJvbGUiOnsiaWQiOiI2MTIzY2YwYWQxMTNhNzAwMTFkMTcyNDUiLCJuYW1lIjoiYWRtaW4iLCJjaGlsZFJvbGVzIjpudWxsfSwiZ3JvdXBzIjpbXSwiaWRTZXNzaW9uIjoiNjFiODk0Mzc1MTVmNmUwMDEwZjBjZDIzIiwiaWF0IjoxNjM5NDg2NTE5LCJleHAiOjE2Mzk1NzI5MTksImp0aSI6IjYxMjNjZjBhZDExM2E3MDAxMWQxNzI1OSJ9.X7Lx4R1pAzWjVtcyeislvpgw_aiRtl-VnEsXDWAbojU'
+/**
+ * 
+ * Parametros usados para obtener info de archivos o subir archivos
+ * URL = http://host:port+path
+ * @param {String} host   Host ej: '192.168.10.33'
+ * @param {Number} port   Puerto ej: 7070
+ * @param {String} path   Path ej: '/api/file'
+ * @param {Array} params  Parametros de la query paginada ej: array(
+ *    'pageNumber' => 1,
+ *    'itemsPerPage'=> 5,
+ *    'search'=> 'nombreImagen',
+ *    'orderBy'=> 'campoDeBaseDeDatos',
+ *    'orderDesc'=> null o True
+ * )
+*/
+const getFiles = async (host, port, path, params = []) => {
+  if (!host || !port || !path) return [{ errorMessage: 'undefined host, port or path' }]
+
   let options = {
     host,
     port,
@@ -11,62 +30,121 @@ const callAPI = async (method, host, port, path, id = null, params = [], filePat
     }
   };
 
-  switch (method) {
-    case 'POST':
-      const FormData = require('form-data')
-      const { request } = require('http')
-      const { createReadStream } = require('fs')
+  return new Promise((resolve, reject) => {
 
-      const readStream = createReadStream('./exampleFiles/prueba.txt')
-      const form = new FormData()
+    get(options, (res) => {
+      let body = ""
+      res.on('data', function (data) {
+        body += data
+      })
+      res.on('end', function () {
+        // Si se ejecutó correctamente devuelvo el objeto con todos los archivos que encontró, dentro de la propiedad items
+        resolve(JSON.parse(body))
+      })
+      res.on('error', function (e) {
+        // Si hubo algún error devuelvo el mensaje de error
+        reject([{ errorMessage: e.message }])
+      })
+    })
+  })
+}
 
-      form.append('file', readStream);
+/**
+  * 
+  * Parametros usados para obtener info de archivos o subir archivos
+  * URL = http://host:port+path+id
+  * @param {String} host   Host ej: '192.168.10.33'
+  * @param {Number} port   Puerto ej: 7070
+  * @param {String} path   Path ej: '/api/file'
+  * @param {String} id     Id del archivo en mongo ej: "61954ca5fcce23001007da16"
+*/
+const getFile = async (host, port, path, id) => {
+  if (!host || !port || !path) return [{ errorMessage: 'undefined host, port or path' }]
+
+  if (id) {
+
+    let options = {
+      host,
+      port,
+      path: `${path}/${id}`,
+      headers: {
+        "Authorization": `Bearer ${tokenStr}`
+      }
+    }
+
+    return new Promise((resolve, reject) => {
+
+      get(options, (res) => {
+        let body = ""
+        res.on('data', function (data) {
+          body += data
+        })
+        res.on('end', function () {
+          // Si se ejecutó correctamente devuelvo el objeto con todos los archivos que encontró, dentro de la propiedad items
+          resolve(JSON.parse(body))
+        })
+        res.on('error', function (e) {
+          // Si hubo algún error devuelvo el mensaje de error
+          reject([{ errorMessage: e.message }])
+        })
+      })
+    })
+  } else {
+    return [{ errorMessage: 'id cannot be empty or null' }]
+  }
+}
+
+/**
+ 
+ * Parametros usados para obtener info de archivos o subir archivos
+ * URL = http://host:port+path+filePath
+ * @param {String} host     Host ej: '192.168.10.33'
+ * @param {Number} port     Puerto ej: 7070
+ * @param {String} path     Path ej: '/api/file'
+ * @param {String} filePath Path relativo del el archivo a subir respecto del archivo actual ej: "./exampleFiles/prueba.txt"
+ 
+ * @return {Array}          Array de objetos con toda la info de los archivos encontrados, el archivo subido o el error
+*/
+const createFile = async (host, port, path, filePath = null) => {
+  if (!host || !port || !path) return [{ errorMessage: 'undefined host, port or path' }]
+
+  if (filePath) {
+    const readStream = createReadStream(filePath)
+    const form = new FormData()
+
+    form.append('file', readStream);
+
+    return new Promise((resolve, reject) => {
 
       const req = request(
         {
           host,
           port,
           path,
-          method,
+          method: 'POST',
           headers: {
             'Authorization': `Bearer ${tokenStr}`,
             ...form.getHeaders()
           }
         },
         response => {
-          return (response.statusCode)
+          resolve([{ message: `StatusCode: ${response.statusCode} - ${response.statusMessage}` }])
         }
       );
 
       try {
-        form.pipe(req).on('error', (e) => console.log(e))
+        form.pipe(req).on('error', (e) => { reject([{ errorMessage: e.message }]) })
       } catch (error) {
-        console.log('e', error)
+        reject([{ errorMessage: error.message }])
       }
-      break;
-    case 'GET':
-      const https = require('http')
-
-      if (id) options.path = `${path}/${id}`
-
-      let body = ""
-      https.get(options, (res) => {
-        res.on('data', (data) => { body += data })
-
-        // Si se ejecutó correctamente devuelvo el objeto con todos los archivos que encontró, dentro de la propiedad items
-        res.on('end', () => { return body })
-
-        // Si hubo algún error devuelvo el mensaje de error
-        res.on('error', (e) => { return `Error: ${e.message}` })
-      })
-
-      break;
-    default:
-      break;
+    })
+  } else {
+    return [{ errorMessage: 'filePath cannot be empty or null' }]
   }
 }
 
-//method, host, port, path, id = null, params = [], filePath = null, result
-//const prueba = callAPI('POST', '192.168.10.33', 7070, '/api/file')
-
-module.exports = callAPI
+module.exports = {
+  getFiles,
+  getFile,
+  createFile
+};
